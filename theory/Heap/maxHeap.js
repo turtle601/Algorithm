8c153ec6292de579b56f1756d1e57aabf84c9798
turtle601 <turtle601@naver.com>
2022-07-30 15:23:36 +0900

    힙 : 최대힙 구현

---
+++ theory/Heap/maxHeap.js
@@ -0,0 +1,54 @@
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  swap(idx1, idx2) {
+    const temp = this.heap[idx1];
+    this.heap[idx1] = this.heap[idx2];
+    this.heap[idx2] = temp;
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let currentIdx = this.heap.length - 1;
+    let parentIdx = Math.floor(currentIdx / 2);
+
+    while (parentIdx !== 0 && this.heap[parentIdx] < value) {
+      this.swap(parentIdx, currentIdx);
+
+      currentIdx = parentIdx;
+      parentIdx = Math.floor(currentIdx / 2);
+    }
+  }
+
+  shift() {
+    if (this.heap.length === 2) return this.heap.pop();
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIdx = 1;
+    let leftIdx = 2;
+    let rightIdx = 3;
+
+    while (
+      this.heap[currentIdx] < this.heap[leftIdx] ||
+      this.heap[currentIdx] < this.heap[rightIdx]
+    ) {
+      if (this.heap[leftIdx] < this.heap[rightIdx]) {
+        this.swap(currentIdx, rightIdx);
+
+        currentIdx = rightIdx;
+      } else {
+        this.swap(currentIdx, leftIdx);
+
+        currentIdx = leftIdx;
+      }
+
+      leftIdx = currentIdx * 2;
+      rightIdx = currentIdx * 2 + 1;
+    }
+
+    return returnValue;
+  }
+}
